17ac5ea7fdf794f2df413d6ffe1a90258e7cdc69
dengmin <minhao123@gmail.com>
2018-02-04 16:21:38 +0800

    clear task history

--- src/main/java/com/rose/scheduler/core/RoseSchedulerInit.java
+++ src/main/java/com/rose/scheduler/core/RoseSchedulerInit.java
@@ -1,12 +1,10 @@
 package com.rose.scheduler.core;
 
+import com.alibaba.fastjson.JSONObject;
 import com.rose.scheduler.common.AppCtx;
-import com.rose.scheduler.core.component.HttpJobComponent;
-import com.rose.scheduler.core.component.JobComponent;
-import com.rose.scheduler.core.component.KettleJobComponent;
-import com.rose.scheduler.core.component.ShellJobComponent;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
+import com.rose.scheduler.common.Constants;
+import com.rose.scheduler.core.component.*;
+import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +15,7 @@ public class RoseSchedulerInit {
         initJobComponent();
 
         startScheduler();
+        initDefaultTask();
     }
 
     private void initJobComponent() throws Exception{
@@ -29,6 +28,27 @@ public class RoseSchedulerInit {
         JobStore.jobs.put(jobComponent.getName(), jobComponent);
     }
 
+    /**
+     * 初始化默认的任务
+     */
+    private void initDefaultTask() throws SchedulerException{
+        Scheduler scheduler = AppCtx.getBean(Scheduler.class);
+        String name = "ClearTaskHistoryJob";
+        String group = "BuildIn";
+        String cron = "0 0 0 * * ?";
+        String description = "内置任务，用于清除历史任务记录";
+        if (!scheduler.checkExists(new JobKey(name, group))) {
+            JobDetail jobDetail = JobBuilder.newJob(BuildInJobComponent.class).withIdentity(name, group).build();
+            JobDataMap dataMap = new JobDataMap();
+            JSONObject taskParam = new JSONObject();
+            taskParam.put("task", "clear_task_history");
+            taskParam.put("keep_days", 7);
+            dataMap.put(Constants.JOB_TASK_PARAM, taskParam.toJSONString());
+
+            CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity(name, group).usingJobData(dataMap).withDescription(description).withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
+            scheduler.scheduleJob(jobDetail, trigger);
+        }
+    }
 
     private void startScheduler(){
         Scheduler scheduler = AppCtx.getBean(Scheduler.class);
--- src/main/java/com/rose/scheduler/core/component/BuildInJobComponent.java
+++ src/main/java/com/rose/scheduler/core/component/BuildInJobComponent.java
@@ -1,8 +1,19 @@
 package com.rose.scheduler.core.component;
 
+import com.alibaba.fastjson.JSONObject;
+import com.rose.scheduler.common.AppCtx;
 import com.rose.scheduler.core.TaskExecContext;
 import com.rose.scheduler.exception.RoseException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.quartz.JobExecutionException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
 
 public class BuildInJobComponent extends JobComponent{
     @Override
@@ -24,6 +35,29 @@ public class BuildInJobComponent extends JobComponent{
 
     @Override
     public boolean run(TaskExecContext context) throws RoseException {
+        JSONObject taskParam = context.getTaskParam();
+        String task = taskParam.getString("task");
+        if (StringUtils.equals("clear_task_history", task)) {
+            int keepDays = taskParam.getInteger("keep_days");
+            Calendar date_point = DateUtils.truncate(Calendar.getInstance(), Calendar.DAY_OF_MONTH);
+            date_point.set(Calendar.DAY_OF_MONTH, date_point.get(Calendar.DAY_OF_MONTH) - keepDays);
+            JdbcTemplate jdbcTemplate = AppCtx.getBean(JdbcTemplate.class);
+            String sql = "DELETE FROM TASK_HISTORY WHERE FIRED_TIME <= ?";
+            jdbcTemplate.execute(sql, new PreparedStatementCallback<Object>() {
+                @Override
+                public Object doInPreparedStatement(PreparedStatement ps) throws DataAccessException {
+                    try {
+                        ps.setLong(1, date_point.getTimeInMillis());
+                        int result = ps.executeUpdate();
+                        context.log("清除任务执行历史， 已经清除 "+result+"条数据!");
+                        return result;
+                    } catch (SQLException e) {
+                        context.log("任务执行失败", e);
+                    }
+                    return null;
+                }
+            });
+        }
         return false;
     }
 }
